6d9a0bc072a0140f6fcd4aa60feafc24e5c5389b
jjsmith32199 <113246711+jjsmith32199@users.noreply.github.com>
2023-02-14 20:41:51 -0500

    initial commit with starter code

---
+++ index.js
@@ -0,0 +1,9 @@
+// Creating the necessary variables for the Inquirer and FS packages
+const inquirer = require('inquirer'); 
+const fs = requier('fs');
+// Variables created to grab info from class js files
+
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
